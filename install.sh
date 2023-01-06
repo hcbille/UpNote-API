9169a55a452b74b033a7c3266563010a14d00a6a
hc <hcbille@gmail.com>
2023-01-06 23:50:12 +0100

    Adding installer and runMe file

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/data/data/com.termux/files/usr/bin/sh
+cp /boot/start-API ~/.termux/boot/start-API
+cp /boot/start-sshd ~/.termux/boot/start-sshd
+chmod +x ~/.termux/boot/start-sshd
+chmod +x ~/.termux/boot/start-API
