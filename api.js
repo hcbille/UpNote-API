d50ac885cd7fb0a5bcde8f2356695e532c1235e7
hcbille <32437363+hcbille@users.noreply.github.com>
2023-01-06 20:44:10 +0100

    Update api.js

--- api.js
+++ api.js
@@ -30,7 +30,7 @@ app.get('/create_note', (req, res) => {
   const xCallbackUrl = `upnote://x-callback-url/note/new?title=${encodeURIComponent(title)}&text=${encodeURIComponent(text)}&notebook=${encodeURIComponent(notebook)}`;
 
   // Run the x-callback-url using the child_process module
-  exec(`xdg-open "${xCallbackUrl}"`, { shell: '/bin/bash' }, (error, stdout, stderr) => {
+  exec(`xdg-open "${xCallbackUrl}"`, { shell: '/data/data/com.termux/files/usr/bin/bash' }, (error, stdout, stderr) => {
     if (error) {
       console.error(`Error running x-callback-url: ${error}`);
       res.status(500).send({ error: 'Internal Server Error' });
