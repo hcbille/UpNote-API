3b6750bac40554a9e0aef3e3ee71bf0023a36b8d
hc <hcbille@gmail.com>
2023-01-08 22:06:41 +0100

    Adding some output to start/stop script

--- runMe.sh
+++ runMe.sh
@@ -1,4 +1,6 @@
 #!/data/data/com.termux/files/usr/bin/bash
+echo "Stopping Service"
 pkill node
 sleep 1
+echo "Starting Service"
 node api.js &
--- stop.sh
+++ stop.sh
@@ -1,2 +1,3 @@
 #!/data/data/com.termux/files/usr/bin/bash
+echo "Stopping Service"
 pkill node
